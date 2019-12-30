f9800a2512e527fab0e489d2ac0593801e15cd7f
JyunpeiKuroda <totoro.black6996@gmail.com>
2019-12-31 03:24:34 +0900

    #1event-create
    親テーブルになるeventに付随する箇所の記述追加。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Event.create({title: 'event1 title', body: 'this is the body of event1'})
+Event.create({title: 'event2 title', body: 'this is the body of event2'})
+Event.create({title: 'event3 title', body: 'this is the body of event3'})
+Event.create({title: 'event4 title', body: 'this is the body of event4'})
+Event.create({title: 'event5 title', body: 'this is the body of event5'})
+Event.create({title: 'event6 title', body: 'this is the body of event6'})
